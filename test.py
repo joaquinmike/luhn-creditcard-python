b6ec5369b766ddfee4597d8b2817406e1b178184
Joaquin Tarazona <jmike410@gmail.com>
2018-11-02 23:55:41 -0500

    update details

--- test.py
+++ test.py
@@ -8,6 +8,6 @@ print(validateCard)
 validateCard = libraryLuhnCreditCard.validCreditCard('4257418986233381','mastercard')
 print(validateCard)
 
-#Card MC Not Valid
+#Card MC Valid
 validateCard = libraryLuhnCreditCard.validCreditCard('5514757572511015','mastercard')
 print(validateCard)
